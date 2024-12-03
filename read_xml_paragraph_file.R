cedb00f293e22083bf294885db68bf0cb37db8a4
Bernhard Angele <bernhardangele@mac-studio-bangele.local>
2024-12-03 13:11:23 +0100

    get character-based regions from XML file

--- read_xml_paragraph_file.R
+++ read_xml_paragraph_file.R
@@ -5,14 +5,14 @@ library(tidyverse)
 my_xml <- read_xml("P3_Word coordinates.xml") 
 
 # Extract all DynamicAOI nodes with Group = "Word"
-word_aois <- xml_find_all(my_xml, "//DynamicAOI[Group = 'Word']")
+character_aois <- xml_find_all(my_xml, "//DynamicAOI[Group = 'Character']")
 
 # Create an empty list to store the extracted data
-word_data <- list()
+character_data <- list()
 
 # Loop through each Word AOI node
-for (i in seq_along(word_aois)) {
-  aoi <- word_aois[i]
+for (i in seq_along(character_aois)) {
+  aoi <- character_aois[i]
   
   # Extract Name
   name <- xml_text(xml_find_first(aoi, "Name"))
@@ -27,7 +27,7 @@ for (i in seq_along(word_aois)) {
   y2 <- as.numeric(xml_text(xml_find_first(points[length(points)], "Y")))
   
   # Store the extracted data in a list
-  word_data[[i]] <- list(Name = name, 
+  character_data[[i]] <- list(Name = name, 
                          X1 = x1, 
                          Y1 = y1, 
                          X2 = x2, 
@@ -35,10 +35,12 @@ for (i in seq_along(word_aois)) {
 }
 
 # Convert the list to a data frame
-word_df <- do.call(rbind, lapply(word_data, as.data.frame))
+character_df <- do.call(rbind, lapply(character_data, as.data.frame)) %>%
+  mutate(width = X2 - X1,
+         height = Y2 - Y1)
 
 # remove duplicates
-word_df <- word_df[!duplicated(word_df),]
+word_df <- character_df[!duplicated(character_df),]
 
 # split the Name column into Paragraph, Section, Word, Character, Region, Line, and string
 # the format of the Name column is "__re_p1_s1_w1_c0_r0_l1_Hay"
@@ -54,11 +56,11 @@ word_df_with_region_info <- word_df %>%
          string = str_extract(Name, "_([^_]+)$")) %>%
   # remove the "p", "s", "w", "c", "r", "l" and "_" from the columns
   mutate(across(c(paragraph, section, word, character, region, line), ~ str_remove(., "[a-z]"))) %>%
-  mutate(string = str_remove(string, "_"))
-
-new_colnames <- c("Name", "char_xmin", "char_ymin", "char_xmax", "char_ymax", 
-                  "paragraph", "section", "word", "character", "region", 
-                  "assigned_line", "char") 
+  mutate(string = str_remove(string, "_")) %>%
+  rename(char_xmin = X1, char_ymin = Y1, char_xmax = X2, char_ymax = Y2,
+         letter = character,
+         assigned_line = line,
+         char = string)
 
 # Assign the new column names to the data frame
 colnames(word_df_with_region_info) <- new_colnames 
